c48d2aedeca1b8f8b54c13e78ba58cb01a1fbbb6
matej.dvorak <cementnatreti@gmail.com>
2024-09-24 12:16:45 +0200

    1

---
+++ src/datove_typy/darove_typy.java
@@ -0,0 +1,12 @@
+package datove_typy;
+
+public class darove_typy {
+
+    public static void main(String[] args) {
+        byte bCislo;
+        int iCislo = 50;
+
+        bCislo = (byte)iCislo;
+         System.out.println(bCislo);
+    }
+}
